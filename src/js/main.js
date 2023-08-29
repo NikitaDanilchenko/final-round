65f858503b95a0f5cdc636a6dd80a91f466d66d3
Nikita Danilchenko <nick92dan98@gmail.com>
2023-08-29 20:06:15 +0300

    commit scss

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 import "../../node_modules/focus-visible/dist/focus-visible";
 import "../scss/main.scss";
 import "../index.html";
+import Swiper from "swiper/bundle";
+
+//Инициализация свайпер под мобильную версию
+if (window.innerWidth < 768) {
+  const swiper = new Swiper(".swiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    spaceBetween: 16,
+  });
+}
